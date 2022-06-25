c8da9c6f82dc57784c7518511e863d05f3dff1c3
sergemalo <serge.malo@orolia.com>
2022-06-25 15:46:06 -0400

    Added unit test

--- nmea.py
+++ nmea.py
@@ -130,6 +130,13 @@ def unitTest():
   expectedPos = Position(37.387458333, -121.97236, 9.0)
   if (unitTestGPGGA("$GPGGA, 161229.487, 3723.2475, N, 12158.3416, W, 1, 07, 1.0, 9.0, M, , , , 0000*18", 16*3600.0 + 12*60.0 + 29.487, expectedPos) == False):
     return False
+  expectedPos = Position(33.5705224283, -112.1842949, 354.682)
+  if (unitTestGPGGA("$GPGGA,001038.00,3334.2313457,N,11211.0576940,W,2,04,5.4,354.682,M,- 26.574,M,7.0,0138*79", 10*60.0 + 38.0, expectedPos) == False):
+    return False
+
+
+
+
 
   print ("UNIT TEST PASS")
   return True
