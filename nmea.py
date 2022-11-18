69c7dd1f1d3df1806dc375f953617c1582d7263f
Serge Malo <sergemalo2000@outlook.com>
2022-11-18 16:31:51 -0500

    Reformat

--- nmea.py
+++ nmea.py
@@ -11,10 +11,10 @@ import math
 # Altitude range: [-Infinite, +Infinite]
 class Position:
   def __init__(self, lat=0.0, long=0.0, alt=0.0):
-    self.lat = lat    
+    self.lat = lat
     self.long = long
     self.alt = alt
-    
+
   def toString(self):
     return "Lat=" + str(self.lat) + "; Long=" + str(self.long) + "; Alt=" + str(self.alt)
 
@@ -29,7 +29,7 @@ def printVersion():
 
 
 class GPGGASentence:
- 
+
   def __init__(self, utcTime = 0.0, lat = 0.0, long = 0.0, fixQual = 0, nbSat = 0, hdop = 0.0, alt = 0.0, heightGeoid = 0.0, timeDGPS = 0, dGPSRefId = 0):
     self.utcTime = utcTime  # Stored as seconds since the beginning of the day (float)
     self.lat = lat
@@ -47,32 +47,32 @@ class GPGGASentence:
 
 
 def parseArgs():
-  
+
   #print (sys.argv)
   opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-  
+
   unitTest = False
   if '-t' in opts: unitTest = True
   return unitTest
 
 
 # GPGGA: Global Positioning System Fix Data
-def parseGPGGA(inSentence):  
-  fields = re.split(r'\,', inSentence) 
+def parseGPGGA(inSentence):
+  fields = re.split(r'\,', inSentence)
   if (fields[0] != "$GPGGA"):
     raise ("Sentence is not GPGGA")
-  
+
   parsedGPGGA = GPGGASentence()
 
-  utcTime = re.match(r"\s*(\d{1,2})(\d{2})(\d{2}\.\d*)", fields[1]) 
+  utcTime = re.match(r"\s*(\d{1,2})(\d{2})(\d{2}\.\d*)", fields[1])
   if (utcTime == None):
     raise ("Unable to parse UTC Time")
   parsedGPGGA.utcTime = float(utcTime.group(1)) * 3600.0
   parsedGPGGA.utcTime = parsedGPGGA.utcTime + float(utcTime.group(2)) * 60.0
   parsedGPGGA.utcTime = parsedGPGGA.utcTime + float(utcTime.group(3))
 
-  
-  latitude = re.match(r"\s*(\d{1,3})(\d{2})(\.)(\d{4})", fields[2]) 
+
+  latitude = re.match(r"\s*(\d{1,3})(\d{2})(\.)(\d{4})", fields[2])
   if (latitude == None):
     raise ("Unable to parse Latitude")
   parsedGPGGA.lat = float(latitude.group(1))
@@ -80,19 +80,19 @@ def parseGPGGA(inSentence):
   if (fields[3] == "S"):
       parsedGPGGA.lat = -parsedGPGGA.lat
 
-  longitude = re.match(r"\s*(\d{1,3})(\d{2})(\.)(\d{4})", fields[4]) 
+  longitude = re.match(r"\s*(\d{1,3})(\d{2})(\.)(\d{4})", fields[4])
   if (longitude == None):
     raise ("Unable to parse Longitude")
   parsedGPGGA.long = float(longitude.group(1))
   parsedGPGGA.long = parsedGPGGA.long + (float(longitude.group(2)) + float(longitude.group(4) )/10000.0 )/60.0
 
-  west = re.match(r"\s*(\w+)", fields[5]) 
+  west = re.match(r"\s*(\w+)", fields[5])
   if (west.group(1) == "W"):
       parsedGPGGA.long = -parsedGPGGA.long
 
   parsedGPGGA.hdop  = float(fields[8])
   parsedGPGGA.alt  = float(fields[9])
-  
+
   return parsedGPGGA
 
 def unitTestGPGGA(sentence, expectedTime, expectedPos):
@@ -103,27 +103,27 @@ def unitTestGPGGA(sentence, expectedTime, expectedPos):
   parsedGPGGA = parseGPGGA(sentence)
   parsedPos.lat = parsedGPGGA.lat
   parsedPos.long = parsedGPGGA.long
-  parsedPos.alt = parsedGPGGA.alt 
-  
+  parsedPos.alt = parsedGPGGA.alt
+
   if (not math.isclose(parsedGPGGA.utcTime, expectedTime, abs_tol=0.0000000001)):
     print ("Invalid UTC Time")
     print ("Expected: "+ str(expectedTime))
     print ("Parsed  : "+ str(parsedGPGGA.utcTime))
     return False
-  
+
   if (not expectedPos.equal(parsedPos)):
     print ("Invalid Position")
     print ("Expected: "+ expectedPos.toString())
     print ("Parsed  : "+ parsedPos.toString())
     return False
-          
+
   return True;
 
 
 def unitTest():
   print ("UNIT TEST")
   print ("Parsing NMEA...")
-  
+
   expectedPos = Position(-43.1234, -73.0, 2.0)
   if (unitTestGPGGA("$GPGGA,120000.000,4307.4040,S,07300.0000,W,1,9,0.91,2.0,M,,M,,*55", 12*3600.0, expectedPos) == False):
     return False
@@ -144,14 +144,10 @@ def unitTest():
 def main():
   printVersion()
   test = parseArgs()
-  
+
   if (test == True):
     if (unitTest() == False):
       print ("UNIT TEST FAIL!")
-    
-    
-    
-    
-    
-    
+
+
 main()
